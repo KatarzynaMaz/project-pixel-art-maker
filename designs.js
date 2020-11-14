103bebab32f51da9d9dbe6b10202fff9e885ea0f
Katarzyna Mazur <mazur66@hotmail.com>
2020-11-14 18:30:40 -0500

    Added comments and defined variables.

--- designs.js
+++ designs.js
@@ -1,3 +1,20 @@
+//Define your variables by selecting the DOM elements that the user will interact with. 
+//This is where your jQuery skills can come into play! For instance, the submit button, the table,
+// and the color picker need to be accessed. The value of the color selected needs to be stored as well,
+// since the clicked cell in the table needs to be set to the selected color.
+
+let width = document.getElementById("inputWidth");
+let height = document.getElementById("inputHeight");
+let submit = document.getElementById("submit");
+let color = document.getElementById("colorPicker");
+//Add event listeners to the relevant DOM elements,
+//so that user input can be color values and table sizes can be dynamically set by the user.
+
+//Set the size of the cross stitch canvas as an _N_ by _M_ grid with the makeGrid() function.
+// Use your knowledge of JavaScript loops to dynamically clear and create the table based on user input. 
+//Each cell should have an event listener that sets the background color of the cell to the selected color.
+
+
 // Select color input
 // Select size input
 
